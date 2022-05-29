6042e963b18f5407a77b912c8ebcbf2741905f5a
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-05-29 14:25:23 +0900

    Fix:watchプロパティを削除

--- main.js
+++ main.js
@@ -7,23 +7,32 @@ var app = new Vue({
     todos: [],
     selectedTodo: null,
   },
+  mounted () {
+    let dataStr = localStorage.getItem(this.storageKey);
+    if (dataStr) {
+      this.todos = JSON.parse(dataStr);
+    }
+  },
 
   methods: {
     storeTodo() {
       if (this.todo == "") return;
 
-      this.todos.push({text: this.todo});
+      this.todos.push({ text: this.todo });
       this.todo = "";
+      localStorage.setItem(this.storageKey, JSON.stringify(this.todos));
     },
 
     removeTodo(index) {
       this.todos.splice(index, 1);
+      localStorage.setItem(this.storageKey, JSON.stringify(this.todos));
     },
 
     updateTodo() {
       this.todos.splice(this.selectedIndex, 1, { text: this.todo });
       this.todo = "";
       this.isEditing = false;
+      localStorage.setItem(this.storageKey, JSON.stringify(this.todos));
     },
 
     editTodo(index, todo) {
@@ -31,19 +40,5 @@ var app = new Vue({
       this.todo = todo.text;
       this.selectedIndex = index;
     },
-  },
-  created: function () {
-    let dataStr = localStorage.getItem(this.storageKey);
-    if (dataStr) {
-      this.todos = JSON.parse(dataStr);
-    }
-  },
-  watch: {
-    todos: {
-      handler: function () {
-        localStorage.setItem(this.storageKey, JSON.stringify(this.todos));
-      },
-      deep: true,
-    },
-  },
+  }
 });
